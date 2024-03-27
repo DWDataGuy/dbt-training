280b2a17414b182ef1cd29381a8b32d54f568264
djwhiting_2000@yahoo.com <djwhiting_2000@yahoo.com>
2024-03-27 17:33:42 +0000

    Added limit data macros

--- models/Staging/stg_orders.sql
+++ models/Staging/stg_orders.sql
@@ -19,10 +19,12 @@ p.productid,
 p.category,
 p.productname,
 p.subcategory,
-{{ markup() }} as markup 
+{{ markup('ordersellingprice','ordercostprice') }} as markup 
 
 from {{ ref('RAW_ORDERS')}} as o
 left join {{ ref('RAW_CUSTOMER') }} as c
 on o.customerid = c.customerid
 left join {{ ref('RAW_PRODUCT') }} as p
 on o.productid = p.productid
+
+{{limit_data_in_dev('orderdate')}} 
